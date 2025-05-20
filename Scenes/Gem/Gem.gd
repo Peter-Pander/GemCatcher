0d405480bdb07a04370b5f6c484157bd382e5ae2
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-05-20 19:46:48 +0200

    Change script, so gems fall off screen
    Also refactor code

--- Scenes/Gem/Gem.gd
+++ Scenes/Gem/Gem.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
+# How fast the gem falls (pixels per second)
+const SPEED: float = 100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass  # nothing special to do on spawn
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# Move the gem downwards
+	position.y += SPEED * delta
+
+	# If it falls below the bottom of the viewport, free it
+	if position.y > get_viewport_rect().end.y:
+		print("Gem falls off")
+		set_process(false)
+		queue_free()
